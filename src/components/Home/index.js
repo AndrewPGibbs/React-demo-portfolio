ac36f35e8ec9285a6ca4debca371231eba5e34f3
Andrew PG <andrewpgibbs71@gmail.com>
2023-02-23 15:32:17 -0500

    adding animations to home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,23 @@
 import LogoTitle from '../../assets/images/logo-s.png';
 import { Link } from 'react-router-dom';
 import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['u', 'p', 'e', 'r']
+    const jobArray = ['d', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi Muggles, I'm
+                <h1>Hi Muggles,
+                    <br /> I'm
                 <br />
                 <img src={LogoTitle} alt="Developer"/>
-                uper great Developer man!
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15}
+                />
                 </h1>
                 <h2> Frontend Developer</h2>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
