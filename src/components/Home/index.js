69d6a0fff8910e981b54d4bd0b8ec006412659e9
Andrew PG <andrewpgibbs71@gmail.com>
2023-02-24 18:48:07 -0500

    adding gsap library animations to Logo on homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,25 @@
-import LogoTitle from '../../assets/images/logo-s.png';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './index.scss';
-import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logo-s.png'
+import Logo from '../Home/Logo'
+import './index.scss'  
+
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = ['u', 'p', 'e', 'r', ' ']
     const jobArray = ['D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+          setLetterClass('text-animate-hover')
+        }, 4000)
+        return () => clearTimeout(timeout)
+      }, [])
+
     return (
+        <>
         <div className="container home-page">
             <div className="text-zone"> 
             <h1>
@@ -30,10 +41,13 @@ const Home = () => {
                 />
                 </h1>
                 <h2> Frontend Developer</h2>
-                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                <Link to="/contact" className="flat-button">CONTACT ME
+                </Link>
             </div>
+            <Logo />
 
         </div>
+        </>
     )
 }
 export default Home;
