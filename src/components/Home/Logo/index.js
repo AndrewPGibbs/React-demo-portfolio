0106a08e39e34a74bac7a55959eec3cd58bd8e94
Andrew PG <andrewpgibbs71@gmail.com>
2023-02-24 19:05:23 -0500

    gsap animations fixed so that the logo is outlined first before becoming solid

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -14,11 +14,28 @@ const Logo = () =>{
 
   useEffect(() => {
     gsap.registerPlugin(DrawSVGPlugin)
+
     gsap.timeline()
     .to(bgRef.current, {
       duration: 1,
-      opacity: 1
+      opacity: 1,
     })
+    .from(outlineLogoRef.current, {
+      drawSVG: 0,
+      duration: 20,
+    })
+
+    gsap.fromTo(
+      solidLogoRef.current,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        delay: 4,
+        duration: 4,
+      }
+    )
   },[])
     return (
 
