d48b223c3eaf60dbceb93d25b9377f8a1615a5d2
Andrew PG <andrewpgibbs71@gmail.com>
2023-03-06 15:55:11 -0500

    portfolio component created and routed

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
+
+    const form = useRef()
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -14,56 +17,90 @@ const Contact = () => {
           return () => clearTimeout(timer)
         }, [])
 
+
+        const sendEmail = (e) => {
+            e.preventDefault()
+
+            emailjs
+             .sendForm(
+                    'service_9dm7cgo',
+                    'template_alt9lpf',
+                    form.current,
+                    'jeqUvNiYOcd8lqI_J'
+             )
+             .then(
+                () => {
+                    alert('Message successfully sent!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, please try again!')
+                }
+             )
+        }
     return (
-        <>
-        <div className='container contact-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters
-                    letterClass={letterClass}
-                    strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
-                    idc={15} />
-                </h1>
-                <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident.
-          </p>
-          <div className='contact-form'>
-            <form>
+      <>
+        <div className="container contact-page">
+          <div className="text-zone">
+            <h1>
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+                idc={15}
+              />
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident.
+            </p>
+            <div className="contact-form">
+              <form ref={form} onSubmit={sendEmail}>
                 <ul>
-                    <li className='half'>
-                        <input type='text'name='name' placeholder='Name' required />
-
-                    </li>
-                    <li className='half'>
-                        <input type='email'name='email' placeholder='Email' required />
-
-                    </li>
-                    <li className=''>
-                        <input type='text'name='subject' placeholder='Subject' required />
-
-                    </li>
-                    <li>
-                        <textarea placeholder='Message' name='message' required></textarea>
-                    </li>
-                    <li>
-                        <input type='submit' className='flat-button' value='SEND' />
-                    </li>
+                  <li className="half">
+                    <input
+                      placeholder="Name"
+                      type="text"
+                      name="name"
+                      required
+                    />
+                  </li>
+                  <li className="half">
+                    <input
+                      placeholder="Email"
+                      type="email"
+                      name="email"
+                      required
+                    />
+                  </li>
+                  <li>
+                    <input
+                      placeholder="Subject"
+                      type="text"
+                      name="subject"
+                      required
+                    />
+                  </li>
+                  <li>
+                    <textarea
+                      placeholder="Message"
+                      name="message"
+                      required
+                    ></textarea>
+                  </li>
+                  <li>
+                    <input type="submit" className="flat-button" value="SEND" />
+                  </li>
                 </ul>
-            </form>
-
-          </div>
-
+              </form>
             </div>
-
+          </div>
         </div>
         <Loader type="pacman" />
-        
-        </>
+      </>
     )
 }
 export default Contact;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -11,63 +11,86 @@ import {
   faHome,
   faUser,
   faEnvelope,
-  faSuitcase,
-  faBars,
-  faClose,
+  faChartPie,
 } from '@fortawesome/free-solid-svg-icons'
 import { Link, NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
-  const [showNav, setShowNav] = useState(false);
+  const [showNav, setShowNav] = useState(false)
 
   return (
-  <div className="nav-bar">
-    <Link className="logo" to="/">
-      <img src={LogoS} alt="logo"></img>
-      <img src={LogoSubtitle} className="sub-logo" alt="logo"></img>
-    </Link>
-    <nav>
-      <NavLink exact="true" activeclassname="active" to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className={'about-link'}
-        to="/about"
-      >
-        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className={'contact-link'}
-        to="/contact"
-      >
-        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-      </NavLink>
-      <NavLink exact="true" activeclassname="active" to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-      </NavLink>
-    </nav>
-    <ul>
+    <div className="nav-bar">
+      <Link className="logo" to="/" onClick={() => setShowNav(false)}>
+        <img src={LogoS} alt="logo"></img>
+        <img src={LogoSubtitle} className="sub-logo" alt="logo"></img>
+      </Link>
+      <nav>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className={'about-link'}
+          to="/about"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className={'portfolio-link'}
+          to="/portfolio"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faChartPie} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className={'contact-link'}
+          to="/contact"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
         <li>
-            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/andrew-gibbs-76a316254/">
-                <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-            </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/andrew-gibbs-76a316254/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
         </li>
         <li>
-            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/andrew-gibbs-76a316254/">
-                <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-            </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/andrew-gibbs-76a316254/"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
         </li>
         <li>
-            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/andrew-gibbs-76a316254/">
-                <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-            </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/andrew-gibbs-76a316254/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
         </li>
-    </ul>
-  </div>
+      </ul>
+    </div>
   )
 }
 export default Sidebar
